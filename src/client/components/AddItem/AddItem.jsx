6fec696e9400c7ad237cb1b687a4bac8f3d91626
Neiman Chen <hahatuke@gmail.com>
2018-08-22 23:35:50 -0700

    axios call to get style categories

--- src/client/components/AddItem/AddItem.jsx
+++ src/client/components/AddItem/AddItem.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { Form, Tab } from 'semantic-ui-react';
 import UploadItem from './UploadItem.jsx';
 import AddUrl from './AddUrl.jsx';
+import { updateStyles } from '../../actions/addItemActions';
 import SearchBarcode from './SearchBarcode.jsx';
 import Axios from 'axios';
 
 export class AddItem extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    Axios.get('/uploaditeminfo')
+    .then((response) => {
+      this.props.actions.updateStyles(response.data);
+    })
+    .catch((err) => {
+      this.props.actions.updateStyles(err);
+    })
   }
 
   render() {
@@ -33,4 +46,12 @@ export class AddItem extends React.Component {
   }
 }
 
-export default withRouter(AddItem)
+const mapStateToProps = state => ({
+  styles: state.addItem.styles
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ updateStyles }, dispatch)
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AddItem));
