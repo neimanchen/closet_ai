018a26bb78adc4378a18ba54bc118f42111faf9d
Neiman Chen <hahatuke@gmail.com>
2018-08-22 23:37:25 -0700

    WIP makes call to server endpoint to save to db

--- src/client/components/AddItem/UploadItemInfo.jsx
+++ src/client/components/AddItem/UploadItemInfo.jsx
@@ -5,14 +5,44 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { updateImageURL } from '../../actions/addItemActions';
 import UploadForm from './UploadForm.jsx';
+import { getFormValues } from 'redux-form';
+import Axios from 'axios';
 
 export class UploadItemInfo extends React.Component {
   constructor(props) {
     super(props)
+    this.submitForm = this.submitForm.bind(this);
   }
 
   submitForm() {
     //TODO: axios call to save to DB
+    // pass style.id in params
+    var item = {
+        brand: this.props.formStates.brand,
+        name: this.props.formStates.itemname,
+        description: this.props.formStates.description,
+        size: this.props.formStates.size,
+        url: this.props.imageURL,
+        price: this.props.formStates.price,
+        category: this.props.formStates.category,
+        color: this.props.formStates.color,
+        date: new Date(this.props.formStates.date)
+      }
+
+    Axios.post('/uploaditem', {
+      item: {
+        brand: this.props.formStates.brand,
+        name: this.props.formStates.itemname,
+        description: this.props.formStates.description,
+        size: this.props.formStates.size,
+        url: this.props.imageURL,
+        price: this.props.formStates.price,
+        category: this.props.formStates.category,
+        color: this.props.formStates.color,
+        date: new Date(this.props.formStates.date)
+      }
+    })
+    console.log('item', item)
   }
 
   render() {
@@ -35,7 +65,7 @@ export class UploadItemInfo extends React.Component {
         <Modal.Content image>
           <Image wrapped size='large' src={this.props.imageURL} />
           <Modal.Description>
-            <UploadForm onSubmit={this.submitForm} />
+            <UploadForm onSubmit={this.submitForm} styles={this.props.styles}/>
           </Modal.Description>
         </Modal.Content>
       </Modal>
@@ -44,7 +74,9 @@ export class UploadItemInfo extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  imageURL: state.addItem.imageURL
+  imageURL: state.addItem.imageURL,
+  styles: state.addItem.styles,
+  formStates: getFormValues('Upload')(state)
 });
 
 const mapDispatchToProps = dispatch => ({
