fc65c76bc3304e294e8cab118cae10f08e650a1e
Neiman Chen <hahatuke@gmail.com>
2018-08-25 17:46:07 -0700

    added start and stop button

--- src/client/components/AddItem/SearchBarcode.jsx
+++ src/client/components/AddItem/SearchBarcode.jsx
@@ -1,37 +1,62 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { updateItem } from '../../actions/addItemActions';
-import { Grid, Input, Button, Form } from 'semantic-ui-react';
+import { updateItem, updateScannedStatus } from '../../actions/addItemActions';
+import { Grid, Input, Button, Form, Header } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 import UploadItemInfo from './UploadItemInfo.jsx';
-import Scanner from './Scanner.jsx'
+import Scanner from './Scanner.jsx';
+import Quagga from 'quagga'
 import Axios from 'axios';
 
 export class SearchBarcode extends React.Component {
   constructor(props) {
     super(props);
+    this.scan = this.scan.bind(this);
+    this.clearItem = this.clearItem.bind(this);
+  }
+
+  scan() {
+    this.props.actions.updateScannedStatus(!this.props.status);
+  }
+
+  clearItem() {
+    this.props.actions.updateItem(null);
   }
 
   render() {
     return (
       <Grid columns="equal">
         <Grid.Column width={14}>
-          <Scanner />
+          <Button onClick={this.scan}>{this.props.status ? 'Stop Scan' : 'Start Scan'}</Button>
+          {this.props.status ? <Scanner /> : null}
+          {
+            this.props.item
+            ?
+            <Grid.Column>
+              <div>
+                <Header as="h2">Scan Success!</Header>
+              </div>
+              <UploadItemInfo />
+              <Button onClick={this.clearItem}>Scan Again</Button>
+            </Grid.Column>
+            :
+            null
+          }
         </Grid.Column>
-        <Scanner />
-        <pre>{JSON.stringify(this.props.item, null, 2)}</pre>
       </Grid>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  item: state.addItem.item
+  item: state.addItem.item,
+  imageURL: state.addItem.imageURL,
+  status: state.addItem.status
 })
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({ updateItem }, dispatch)
+  actions: bindActionCreators({ updateItem, updateScannedStatus }, dispatch)
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchBarcode))
